d7724f17f590763cfa18934cc005e60b81894b06
Nelly <$>
2022-11-14 16:01:29 -0500

    added twitter buttone variabel to js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const spinner = document.querySelector('#js-spinner');
 const newQuoteButton = document.querySelector('#js-new-quote');
+const twitterButton = document.querySelector('#js-tweet');
 newQuoteButton.addEventListener('click', getQuote);
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
