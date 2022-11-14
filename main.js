a3d3f5442749c632fe1dd73745bc9311a1502055
Nelly <$>
2022-11-14 15:35:00 -0500

    checking to make sure proper message dispalyed in consle after clicking button

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
+
+function getQuote() {
+    console.log("quote button was clicked");
+  }
