72bfcff723800a571ac98e106e65dc770da267d3
Nelly <$>
2022-11-14 15:56:45 -0500

    modified qet quote function to incorporate spinner effect

--- main.js
+++ main.js
@@ -4,24 +4,25 @@ newQuoteButton.addEventListener('click', getQuote);
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 async function getQuote() {
-    // The `try` block executes the statements within it as usual.
-    // If an exception is thrown, the statements defined in
-    // the `catch` block will be executed.
-    // Learn more here: https://javascript.info/try-catch
+    // remove the "hidden" class on the spinner
+    spinner.classList.remove('hidden');
+    // disable the quote button
+    newQuoteButton.disabled = true;
+  
     try {
       const response = await fetch(endpoint)
-      // If the response is not 200 OK...
       if (!response.ok) {
-        // ...throw an error. This causes control flow
-        // to skip to the `catch` block below.
-        throw Error(response.statusText)
+        throw Error(response.statusText);
       }
-  
       const json = await response.json();
       displayQuote(json.message);
-    } catch (err) {
-      console.log(err)
+    } catch {
       alert('Failed to fetch new quote');
+    } finally {
+      // enable the quote button
+      newQuoteButton.disabled = false;
+      // add the "hidden" class back again
+      spinner.classList.add('hidden');
     }
   }
 //code that displays the quote to the user is placed
