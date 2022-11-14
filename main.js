3154d3b0ccc69a7e49ec21666299ef4bde9d6215
Nelly <$>
2022-11-14 15:37:29 -0500

    added endpoint variable for api

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 function getQuote() {
     console.log("quote button was clicked");
